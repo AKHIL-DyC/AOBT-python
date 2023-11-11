e017dc42337b394e373ee4f1b094ce76e61be124
AKHIL-DyC <akhiljee666@gmail.com>
2023-11-12 04:59:52 +0530

    adding news

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,session, request, render_template
+from flask import Flask, session, request, render_template
 from flask_session import Session
 from langchain.llms import OpenAI
 import openai
@@ -8,24 +8,41 @@ from dotenv import load_dotenv
 import speech_recognition as sr
 import pyttsx3
 import threading
-
+import keyboard
+import requests
+import pythoncom
+flag = 0
 app = Flask(__name__)
-app.config["SESSION_TYPE"]="filesystem"
-
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+def ttse(text):
+    engine = pyttsx3.init(driverName='sapi5')
+    engine.setProperty('voice', 'com.apple.speech.synthesis.voice.karen')
+    engine.setProperty('rate', 150)
+    engine.setProperty('pitch', 1.0)
+    engine.setProperty('volume', 1.0)
+    engine.say(text)
+    engine.runAndWait()
+pythoncom.CoUninitialize()   
 def slow_loading_function():
-    """Simulates a time consuming process (wait 10 seconds and return a reversed string)"""
+    """Simulates a time-consuming process (wait 10 seconds and return a reversed string)"""
     time.sleep(1)
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
 @app.route("/loading", methods=["POST"])
 def loading():
     if request.method == "POST":
         # We'll use a session object to save the data sent by the user for processing
-        #session["user_data"] = request.form.get("user_data")
+        # session["user_data"] = request.form.get("user_data")
         return render_template("loading.html")
-    
+
+
 @app.route("/ai")
 def ai():
     # Finally, use the user data in some intensive process
@@ -59,6 +76,7 @@ def ai():
 
     return render_template('ai.html', answer=answer, question=question, speak=speak)
 
+
 def tts(text):
     engine = pyttsx3.init(driverName='sapi5')
     engine.setProperty('voice', 'com.apple.speech.synthesis.voice.karen')
@@ -68,7 +86,35 @@ def tts(text):
     engine.say(text)
     engine.runAndWait()
 
-#@app.route()
+
+@app.route("/news")
+def news_route():
+    global flag
+    if request.method == "GET":
+        newskey = os.getenv("news_key")
+
+        if newskey is None:
+            print("API key 'news_key' not found in your .env file.")
+            return
+
+        main_url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=" + newskey
+        news_data = requests.get(main_url).json()
+        articles = news_data.get("articles", [])
+
+        news_article = [arti.get('title', '') for arti in articles]
+
+        print("News started. Press 'Spacebar' to end.")
+
+        nws = ""  # Initialize nws variable
+
+        for i in range(10):
+            if flag == 1:
+                break
+            nws += news_article[i] + "   "
+            print(news_article[i])
+            text = news_article[i]
+            threading.Thread(target=ttse, args=(nws,)).start()
+
+        return render_template('news.html', news=nws)
 if __name__ == "__main__":
     app.run(debug=True)
-
