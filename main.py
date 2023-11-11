2a8c0649fe3779d03c70f63dec368291ba5c6e90
AKHIL-DyC <akhiljee666@gmail.com>
2023-11-11 12:01:18 +0530

    modified main also added flask

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
+from flask import Flask, render_template
 from langchain.llms import OpenAI
 import openai
 import os
-from dotenv import main
-from langchain.llms import openai
+from dotenv import load_dotenv
 import speech_recognition as sr
 import pyttsx3
-main.load_dotenv()
-api = os.getenv("API_KEY")
-print(api)
-recognizer = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak something...")
-    recognizer.adjust_for_ambient_noise(source)
-    audio = recognizer.listen(source, timeout=5)
-try:
-    text = recognizer.recognize_google(audio)
-    print("You said: " + text)
-except sr.UnknownValueError:
-    print("Sorry, I could not understand what you said.")
-except sr.RequestError as e:
-    print(f"Could not request results; {e}")
+import threading
 
-llm = OpenAI(openai_api_key=os.getenv("API_KEY"))
-# print(llm.predict(text))
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    load_dotenv()
+
+    api = os.getenv("API_KEY")
+    print(api)
+
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak something...")
+        speak="I am listening"
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, timeout=5)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said: " + text)
+        question=text
+    except sr.UnknownValueError:
+        print("Sorry, I could not understand what you said.")
+    except sr.RequestError as e:
+        print(f"Could not request results; {e}")
+
+    llm = OpenAI(openai_api_key=os.getenv("API_KEY"))
+
+    answer = llm.predict(text)
+    print(answer)
+
+    
+    threading.Thread(target=tts, args=(answer,)).start()
+
+    
+    return render_template('index.html',answer=answer,question=question,speak=speak)
 
 def tts(text):
     engine = pyttsx3.init(driverName='sapi5')
@@ -34,5 +52,5 @@ def tts(text):
     engine.say(text)
     engine.runAndWait()
 
-
-tts(llm.predict(text))
+if __name__ == "__main__":
+    app.run(debug=True)
